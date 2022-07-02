1a09999e0d55b80114138f71890273dc00dba633
anadelarue <annaruiz982@gmail.com>
2022-07-02 18:56:20 +0200

    first commit

---
+++ public/js/botonesMapa.js
@@ -0,0 +1,10 @@
+const myFunction = () => {
+    document.getElementById("first").style.display ='block';
+    document.getElementById("second").style.display ='none'     
+  }
+  
+  const myFunction2 = () => {
+    document.getElementById("second").style.display ='block'
+    document.getElementById("first").style.display ='none'
+  }
+  
